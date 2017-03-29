65b04d2dd7971ef30cddc3bce557469b0c03eca7
Joe Holt <wynatoej@gmail.com>
2017-03-28 22:54:56 -0400

    Added new TableView for a display of tasks

---
+++ BetterReminders/TaskTableVC.swift
@@ -0,0 +1,25 @@
+//
+//  TaskTableVC.swift
+//  BetterReminders
+//
+//  Created by Joe Holt on 3/28/17.
+//  Copyright © 2017 Joe Holt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskTableVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
